5deb8b0095de289494c7d8ea9d06677e51467cf2
OdeliaTorjman <odeliaabbou770@gmail.com>
2024-07-31 22:31:59 +0300

    Initial Commit

---
+++ FunPlaces2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FunPlaces2
+//
+//  Created by CHMITOUZ on 31/07/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
