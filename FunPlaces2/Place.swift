ae80a0f94a99cd490178482d82ae12f00d159bdd
OdeliaTorjman <156775579+OdeliaTorjman@users.noreply.github.com>
2024-08-27 21:56:52 +0300

    Update Place.swift

--- FunPlaces2/Place.swift
+++ FunPlaces2/Place.swift
@@ -4,11 +4,11 @@ import Firebase
 class Place {
     var title: String?
     var description: String?
-    var cost: Double?
+    var cost: Int?
     var address: String?
     var imageUrl: URL?
     
-    init(title: String? = nil, description: String? = nil, cost: Double? = nil, address: String? = nil, imageUrl: URL? = nil) {
+    init(title: String? = nil, description: String? = nil, cost: Int? = nil, address: String? = nil, imageUrl: URL? = nil) {
         self.title = title
         self.description = description
         self.cost = cost
@@ -20,13 +20,14 @@ class Place {
         let value = snapshot.value as? [String: Any] ?? [:]
         self.title = value["title"] as? String
         self.description = value["description"] as? String
-        if let costValue = value["cost"] as? Double {
+        if let costValue = value["cost"] as? Int {
             self.cost = costValue
-        } else if let costString = value["cost"] as? String, let costDouble = Double(costString) {
-            self.cost = costDouble
+        } else if let costString = value["cost"] as? String, let costInt = Int(costString) {
+            self.cost = costInt
         } else {
-            self.cost = 0.0
+            self.cost = 0
         }
+
         self.address = value["address"] as? String
         if let imageUrlString = value["imageURL"] as? String {
             self.imageUrl = URL(string: imageUrlString)
